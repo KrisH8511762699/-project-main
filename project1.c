1038e4426c7ed4010360906216412104271c444a
KrisH8511762699 <146726398+KrisH8511762699@users.noreply.github.com>
2023-10-04 23:32:24 +0530

    Add files via upload

---
+++ project1.c
@@ -0,0 +1,95 @@
+// 4 . program complete
+
+#include<stdio.h>
+
+int main(){
+
+		int a=100;
+		int b;
+		int d;
+		int f;
+		int x;
+		printf("base salary is 100 rs.\n");
+		
+		b = a*10/100+100;
+		printf("hra salary gross net salary is :- %d\n",b);
+		
+		d = a*5/100+100;
+		printf("da salary gross net salary is :- %d\n",d);
+		
+		f = a*8/100+100;
+		printf("ta salary gross net salary is :- %d\n",f);
+		
+		x = b+d+f;
+		printf("total salary is :- %d\n",x);
+		
+		return 0;
+}
+
+//********************************************************************************************************************************************************************************************************************
+// 3. program complete
+
+#include<stdio.h>
+
+int main()
+{
+	float cel;
+	float fen;
+	printf("the program about this cels. to fen. \n");
+	
+	printf("enter the cel. :- ");
+	scanf("%f",&cel);
+	
+	fen = (cel*9/5)+32;
+	printf("the fen is :- %f \n", fen);
+	
+	return 0;
+	
+}
+
+//********************************************************************************************************************************************************************************************************************
+// 1. program complete
+
+#include<stdio.h>
+
+int main(){
+    
+    printf("\t\t*\t\t*\n");
+    printf("\t*\t\t*\n");
+    printf("*\t\t*\n");
+    printf("\t*\t\t*\n");
+    printf("\t\t*\t\t*\n");
+	
+	
+	printf("\t\t\t*\t*\t*\t*\t*\t*\t*\n");
+	printf("*\t*\t*\t*\t*\t*\t*\t*\n");
+	printf("*\t\t*\t\t*\t\t\t*\n");
+	printf("*\t\t*\t\t*\t*\t*\t*\n");
+	printf("*\t\t*\t\t*\t\t\t*\n");
+	printf("*\t*\t*\t*\t*\t*\t*\t*\n");
+	return 0;
+	
+}
+
+//********************************************************************************************************************************************************************************************************************
+//2. program complete
+
+#include <stdio.h>
+
+int main()
+{
+	
+	int a=10;
+	int b=20;
+	
+	printf("%d\n",a);
+	printf("%d\n\n",b);
+	
+    a,b = b,a;
+	printf("%d\n",b);
+	printf("%d\n",a);
+	
+	return 0;
+}
+
+//********************************************************************************************************************************************************************************************************************
